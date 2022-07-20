b28dd94e36177df8015561b366b9b636dbffba02
Jade1724 <haruka.i1724@gmail.com>
2022-07-20 14:39:47 +1200

    Fixed the signal handler for interaction area entered.

--- scripts/InteractionSelection.gd
+++ scripts/InteractionSelection.gd
@@ -4,20 +4,8 @@ extends Spatial
 onready var interaction1Mesh = $Interaction1/Area/MeshInstance
 	
 
-func _on_interaction1_area_entered():
+func _on_interaction1_area_entered(area):
 	#get_node("../OutputNode/Viewport/OtherLabel").text = "ON"
 	var material = interaction1Mesh.get_surface_material(0)
 	material.albedo_color = Color(1, 0, 0)
 	interaction1Mesh.set_surface_material(0, material)
-	
-func _process(delta):
-	var areas = $Interaction1/Area.get_overlapping_areas()
-	get_node("../OutputNode/Viewport/OtherLabel").text = areas as String
-	if areas.size() > 0:
-		var material = interaction1Mesh.get_surface_material(0)
-		material.albedo_color = Color(1, 0, 0)
-		interaction1Mesh.set_surface_material(0, material)
-	else:
-		var material = interaction1Mesh.get_surface_material(0)
-		material.albedo_color = Color(1, 1, 1)
-		interaction1Mesh.set_surface_material(0, material)
