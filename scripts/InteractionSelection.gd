a0a9a4c5c72619ce8750bdf831b3705c2b6a9625
Rutger van Kruiningen <rutgervk.live@gmail.com>
2022-07-20 15:01:50 +1200

    added in two more spheres to select the other two interaction mechanics

--- scripts/InteractionSelection.gd
+++ scripts/InteractionSelection.gd
@@ -2,10 +2,39 @@ extends Spatial
 
 
 onready var interaction1Mesh = $Interaction1/Area/MeshInstance
+onready var interaction2Mesh = $Interaction2/Area/MeshInstance
+onready var interaction3Mesh = $Interaction3/Area/MeshInstance
 	
 
 func _on_interaction1_area_entered(area):
-	#get_node("../OutputNode/Viewport/OtherLabel").text = "ON"
+	resetMeshes()
 	var material = interaction1Mesh.get_surface_material(0)
 	material.albedo_color = Color(1, 0, 0)
 	interaction1Mesh.set_surface_material(0, material)
+
+
+func _on_interaction2_area_entered(area):
+	resetMeshes()
+	var material = interaction2Mesh.get_surface_material(0)
+	material.albedo_color = Color(1, 0, 0)
+	interaction2Mesh.set_surface_material(0, material)
+
+func _on_interaction3_area_entered(area):
+	resetMeshes()
+	var material = interaction3Mesh.get_surface_material(0)
+	material.albedo_color = Color(1, 0, 0)
+	interaction3Mesh.set_surface_material(0, material)
+	
+	
+func resetMeshes():
+	var material = interaction1Mesh.get_surface_material(0)
+	material.albedo_color = Color(1, 1, 1)
+	interaction1Mesh.set_surface_material(0, material)
+	
+	material = interaction2Mesh.get_surface_material(0)
+	material.albedo_color = Color(1, 1, 1)
+	interaction2Mesh.set_surface_material(0, material)
+	
+	material = interaction3Mesh.get_surface_material(0)
+	material.albedo_color = Color(1, 1, 1)
+	interaction3Mesh.set_surface_material(0, material)
