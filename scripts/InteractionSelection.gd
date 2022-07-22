b57c5d44e4ae71277f9be28c65a89c068120153c
Rutger van Kruiningen <rutgervk.live@gmail.com>
2022-07-23 11:18:28 +1200

    Added ability to add more interactables to the scene with area object

--- scripts/InteractionSelection.gd
+++ scripts/InteractionSelection.gd
@@ -9,20 +9,20 @@ onready var interaction3Mesh = $Interaction3/Area/MeshInstance
 func _on_interaction1_area_entered(area):
 	resetMeshes()
 	var material = interaction1Mesh.get_surface_material(0)
-	material.albedo_color = Color(1, 0, 0)
+	material.albedo_color = Color(0, 1, 0)
 	interaction1Mesh.set_surface_material(0, material)
 
 
 func _on_interaction2_area_entered(area):
 	resetMeshes()
 	var material = interaction2Mesh.get_surface_material(0)
-	material.albedo_color = Color(1, 0, 0)
+	material.albedo_color = Color(0, 1, 0)
 	interaction2Mesh.set_surface_material(0, material)
 
 func _on_interaction3_area_entered(area):
 	resetMeshes()
 	var material = interaction3Mesh.get_surface_material(0)
-	material.albedo_color = Color(1, 0, 0)
+	material.albedo_color = Color(0, 1, 0)
 	interaction3Mesh.set_surface_material(0, material)
 	
 	
--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,1 +1,35 @@
 extends Spatial
+
+onready var resetAreaMesh = $InteractionSelection/Reset/Area/MeshInstance
+onready var resetArea = $InteractionSelection/Reset/Area
+onready var canReset = true
+onready var interactablesResourse = load("res://scenes/Interactables.tscn")
+
+
+func _on_reset_area_entered(area):
+	var material = resetAreaMesh.get_surface_material(0)
+	material.albedo_color = Color(1, 0, 0)
+	resetAreaMesh.set_surface_material(0, material)
+	
+	if canReset:
+		addInteractables()
+		canReset = false
+		
+
+
+func _on_reset_area_exited(area):
+	if resetArea.get_overlapping_areas().size() == 0:
+		var material = resetAreaMesh.get_surface_material(0)
+		material.albedo_color = Color(1, 1, 1)
+		resetAreaMesh.set_surface_material(0, material)
+		get_node("OutputNode/Viewport/GripLabel").text = ""
+		canReset = true
+
+
+func addInteractables():
+	add_child(interactablesResourse.instance())
+	
+	
+
+func _process(delta):
+	get_node("OutputNode/Viewport/OtherLabel").text = canReset as String
