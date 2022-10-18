38a86cc5083c937cc98a2b21d71585016de8ee78
Rutger van Kruiningen <rutgervk.live@gmail.com>
2022-10-19 11:19:13 +1300

    removed a single character

--- scripts/InteractionSelection.gd
+++ scripts/InteractionSelection.gd
@@ -11,7 +11,7 @@ var interactionDescriptions = [
 	"This interaction mechanic uses areas on the fingertips to sense when they are interacting with objects. To grip an object simply make sure your thumb and at least one other finger is on or in the object you want to pick up. This can be easily achieved using a pinching motion.\n\nRemember you can use both hands!\n\nPick off all of the Green Apples while leaving the Red Apples"]
 
 func _ready():
-	get_tree().root.get_node("Main/InformationNode/Viewport/TitleLabel").text = "s"
+	get_tree().root.get_node("Main/InformationNode/Viewport/TitleLabel").text = ""
 	get_tree().root.get_node("Main/InformationNode/Viewport/BottomLabel").text = ""
 	
 func _process(delta):
